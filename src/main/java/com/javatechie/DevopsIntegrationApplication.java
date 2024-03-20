e6f13b3cfbce0458fc3cdd62f6415a45c154a1e9
Hashir-Akram <hashirakram197@gmail.com>
2024-03-20 09:12:11 +0530

    a simple java project

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to HummingBirds System and We are exploring about DevOps";
 	}
 
 	public static void main(String[] args) {
